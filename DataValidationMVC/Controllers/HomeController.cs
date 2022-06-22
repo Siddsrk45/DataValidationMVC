d465c27e809e7f89f6db1544966fd2346f6287fa
Siddhesh Kumbhar <siddhu.kumbhar2000@gmail.com>
2022-06-22 18:06:49 +0530

    Add project files.

---
+++ DataValidationMVC/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataValidationMVC.Models;
+
+namespace DataValidationMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SubmitData(Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ DataValidationMVC/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataValidationMVC.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "please enter proper Name ")]
+       
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [Range(18,100)]
+        public int Age { get; set; }
+    }
+}
